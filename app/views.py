fc409cb705998ceffea444396523f5617813f059
Vitor Viel <75342081+VitorViel@users.noreply.github.com>
2022-02-24 14:01:40 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 
-# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+# from rest_framework.decorators import api_view
 # from rest_framework.views import APIView
+# from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework import status
-# from rest_framework import generics
 
 from app.models import Todo
 from app.serializers import TodoSerializer
 
-# Create your views here.
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = todo.objects.all()
