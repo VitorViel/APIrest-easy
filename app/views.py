f704f7e266fdd52aa53fe591ba9692d82d88a581
Vitor Viel <75342081+VitorViel@users.noreply.github.com>
2022-02-24 13:59:16 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+# from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework import status
+# from rest_framework import generics
 
 from app.models import Todo
 from app.serializers import TodoSerializer
@@ -11,5 +14,14 @@ from app.serializers import TodoSerializer
 # Create your views here.
 
 class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
+    queryset = todo.objects.all()
     serializer_class = TodoSerializer
+
+# class TodoListandCreate(generics.ListCreateAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+
+
+# class TodoListUpdateandDelete(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
